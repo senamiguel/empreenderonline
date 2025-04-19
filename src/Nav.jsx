4271edbdff0c4a02fdd1e7e1478ac5a816f97da1
Miguel Sena <miguelaugustosena@gmail.com>
2025-04-19 12:13:13 -0300

    adding responsiveness

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -2,14 +2,14 @@ import './styles/Nav.css'
 
 function Nav() {
   return (
-    <nav>
+    <nav aria-label="Navegação principal">
       <ul>
-        <li>Empreenda.online</li>
+        <li><a href="/" aria-label="Página inicial">Empreenda.online</a></li>
         <div className="nav__btns">
-          <li><a href="">Sobre</a></li>
-          <li><a href="">Contato</a></li>
-          <li><a href="">Login</a></li>
-          <li className="nav__btns__start"><a href="">Começar</a></li>
+          <li><a href="#sobre" aria-label="Sobre nós">Sobre</a></li>
+          <li><a href="#contato" aria-label="Entre em contato">Contato</a></li>
+          <li className="nav__btns__start"><a href="#comecar" aria-label="Comece agora">Começar</a></li>
+          <li><a href="#login" aria-label="Faça login">Login</a></li>
         </div>
       </ul>
     </nav>
